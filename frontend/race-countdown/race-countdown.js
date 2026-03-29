ac3278fff417ded35eebb164aaa9a0e070d5876f
Elvis Rüütel <elvis.ruutel@outlook.com>
2026-03-29 16:30:26 +0000

    race-countdown improvements

--- frontend/race-countdown/race-countdown.js
+++ frontend/race-countdown/race-countdown.js
@@ -3,6 +3,39 @@ const timer = document.getElementById("timer")
 
 let endTime = null
 
+let hideTimer
+
+document.addEventListener("mousemove", () => {
+    document.body.style.cursor = "default"
+
+    clearTimeout(hideTimer)
+    hideTimer = setTimeout(() => {
+        if (document.fullscreenElement) {
+            document.body.style.cursor = "none"
+        }
+    }, 2000)
+})
+
+const fsBtn = document.getElementById("fullscreen-btn")
+
+fsBtn.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+})
+
+document.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) {
+        document.body.classList.add("fullscreen-mode")
+        fsBtn.style.display = "none"
+    } else {
+        document.body.classList.remove("fullscreen-mode")
+        fsBtn.style.display = "block"
+    }
+})
+
 socket.on("state:update", (state) => {
 
     if (state.timer?.running) {
